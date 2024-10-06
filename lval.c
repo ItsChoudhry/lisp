756cb09416fb9e6d62fc1b6d24485cf3e2c71e1f
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2024-10-07 00:10:43 +0100

    I'm done with this shit dog water book

--- lval.c
+++ lval.c
@@ -405,7 +405,7 @@ lenv *lenv_new(void) {
   return e;
 }
 
-void *lenv_del(lenv *e) {
+void lenv_del(lenv *e) {
   for (int i = 0; i < e->count; i++) {
     free(e->syms[i]);
     lval_del(e->vals[i]);
@@ -413,4 +413,31 @@ void *lenv_del(lenv *e) {
   free(e->syms);
   free(e->vals);
   free(e);
+}
+
+lval *lenv_get(lenv *e, lval *k) {
+  for (int i = 0; i < e->count; i++) {
+    if (strcmp(e->syms[i], k->sym) == 0) {
+      return lval_copy(e->vals[i]);
+    }
+  }
+  return lval_err("unbound symbol!");
+}
+
+void lenv_put(lenv *e, lval *k, lval *v) {
+  for (int i = 0; i < e->count; i++) {
+    if (strcmp(e->syms[i], k->sym) == 0) {
+      lval_del(e->vals[i]);
+      e->vals[i] = lval_copy(v);
+      return;
+    }
+  }
+
+  e->count++;
+  e->vals = realloc(e->vals, sizeof(lval *) * e->count);
+  e->syms = realloc(e->syms, sizeof(char *) * e->count);
+
+  e->vals[e->count - 1] = lval_copy(v);
+  e->syms[e->count - 1] = malloc(strlen(k->sym) + 1);
+  strcpy(e->syms[e->count - 1], k->sym);
 }
--- lval.h
+++ lval.h
@@ -54,4 +54,6 @@ lval *builtin_join(lval *a);
 lval *builtin(lval *a, char *func);
 
 lenv *lenv_new(void);
-void *lenv_del(lenv *e);
+void lenv_del(lenv *e);
+lval *lenv_get(lenv *e, lval *k);
+void lenv_put(lenv *e, lval *k, lval *v);
